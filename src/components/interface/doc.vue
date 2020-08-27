18e36459d165de3f4d7ee83e6bb540c72c13f27f
v_llongfang <v_llongfang@tencent.com>
2020-08-27 14:43:12 +0800

    代码优化

--- src/components/interface/doc.vue
+++ src/components/interface/doc.vue
@@ -208,7 +208,7 @@
                 this.scrollTop = func.get_scroll_top();
                 document.addEventListener('click', this.foo);
                 menu.style.left = tree.offsetWidth + 50 + 'px';
-                menu.style.top = MouseEvent.clientY + this.scrollTop - 220 + 'px';
+                menu.style.top = MouseEvent.clientY + this.scrollTop - 150 + 'px';
                 this.categoryModel = object;
             },
             /**
